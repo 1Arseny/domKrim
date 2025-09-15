dad1ebc73e5465380bba7e19dce395c5aae9d3a3
F1zge <arsenijbaulin@yandex.ru>
2025-09-15 13:19:07 +0300

    новый билд

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite'
-
-function resolveBase() {
-  // локально и в превью — корень
-  let base = '/'
-
-  // в GitHub Actions у нас есть GITHUB_REPOSITORY=owner/repo
-  if (process.env.GITHUB_ACTIONS === 'true' && process.env.GITHUB_REPOSITORY) {
-    const repo = process.env.GITHUB_REPOSITORY.split('/')[1]
-    base = `/${repo}/`
-  }
-
-  // Перезаписать base вручную можно через VITE_BASE
-  if (process.env.VITE_BASE) base = process.env.VITE_BASE
-
-  return base
-}
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: resolveBase(),
+  base: process.env.VITE_BASE ?? '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+      },
+    },
+  },
 })
